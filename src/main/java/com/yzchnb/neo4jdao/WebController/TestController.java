f619781559b9110aa83f8ca50c1813ce45e0ad28
Sky-1751862 <564318045@qq.com>
2019-12-19 14:01:20 +0800

    cloud config

---
+++ src/main/java/com/yzchnb/neo4jdao/WebController/TestController.java
@@ -0,0 +1,13 @@
+package com.yzchnb.neo4jdao.WebController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Neo4j connection completed.";
+    }
+}
